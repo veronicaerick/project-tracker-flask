f5fbc3a7f21051fcffe2f6c76d8f5818406fb5d6
Hackbright Student <info@hackbrightacademy.com>
2016-04-27 12:39:10 -0700

    added grades to project listing

--- hackbright.py
+++ hackbright.py
@@ -97,6 +97,7 @@ def get_grades_by_github(github):
     for row in rows:
         print "Student %s received grade of %s for project %s" % (
             github, row[1], row[0])
+    #print "this is rows, ", rows
     return rows
 
 def get_grades_by_title(title):
